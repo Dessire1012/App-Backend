437fd49c94b34aacb731971981589e512746fc8d
Dessire1012 <dessire@unitec.edu>
2024-09-11 09:44:57 -0600

    Cookies de nuevo

--- index.js
+++ index.js
@@ -130,8 +130,21 @@ app.get(
     const userId = req.user.user_id;
     console.log("Setting cookie with userId:", userId);
 
-    res.json({ userId });
+    res.clearCookie("userId", {
+      domain: ".vanguardchat.netlify.app",
+      path: "/",
+    });
 
+    res.cookie("userId", userId, {
+      httpOnly: false,
+      secure: true,
+      sameSite: "None",
+      domain: ".vanguardchat.netlify.app",
+    });
+
+    console.log("Cookies being set:", res.getHeader("Set-Cookie"));
+
+    // Redirect to chatbot page
     res.redirect("https://vanguardchat.netlify.app/chatbot");
   }
 );
